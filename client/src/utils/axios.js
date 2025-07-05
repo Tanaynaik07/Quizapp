01b039fda1728f9e0c491b2f13de491f4b1547f5
Tanay <tanaynaik0710@gmail.com>
2025-07-05 23:00:08 +0530

    change in axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
+
 const axiosInstance = axios.create({
   baseURL: 'https://quiz-app-api-itz3.onrender.com', // or use env variable
   withCredentials: true,
 });
+
+export default axiosInstance;
